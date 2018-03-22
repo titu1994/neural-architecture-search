60df3f4e7df6139ab6ffe6ec66fdf75de9ef0ac4
Somshubra Majumdar <titu1994@gmail.com>
2018-03-21 21:55:37 -0500

    Add NASRNN layer

--- nascell.py
+++ nascell.py
@@ -4,7 +4,9 @@ from keras import initializers
 from keras import regularizers
 from keras import constraints
 from keras import backend as K
-#from keras.layers import RNN
+from keras.layers import RNN
+
+import warnings
 
 #import tensorflow as tf
 #import tensorflow.contrib.rnn as rnn
@@ -424,3 +426,338 @@ class NASCell(Layer):
             if training is None:
                 h._uses_learning_phase = True
         return h, [h, c]
+
+    def get_config(self):
+        config = {'units': self.units,
+                  'projection_units': self.projection_units,
+                  'activation': activations.serialize(self.activation),
+                  'recurrent_activation': activations.serialize(self.recurrent_activation),
+                  'projection_activation': activations.serialize(self.projection_activation),
+                  'use_bias': self.use_bias,
+                  'kernel_initializer': initializers.serialize(self.kernel_initializer),
+                  'recurrent_initializer': initializers.serialize(self.recurrent_initializer),
+                  'projection_initializer': initializers.serialize(self.projection_initializer),
+                  'bias_initializer': initializers.serialize(self.bias_initializer),
+                  'unit_forget_bias': self.unit_forget_bias,
+                  'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
+                  'recurrent_regularizer': regularizers.serialize(self.recurrent_regularizer),
+                  'projection_regularizer': regularizers.serialize(self.projection_regularizer),
+                  'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+                  'kernel_constraint': constraints.serialize(self.kernel_constraint),
+                  'recurrent_constraint': constraints.serialize(self.recurrent_constraint),
+                  'projection_constraint': constraints.serialize(self.projection_constraint),
+                  'bias_constraint': constraints.serialize(self.bias_constraint),
+                  'dropout': self.dropout,
+                  'recurrent_dropout': self.recurrent_dropout,
+                  'implementation': self.implementation}
+        base_config = super(NASCell, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+class NASRNN(RNN):
+    """Neural Architecture Search (NAS) recurrent network cell.
+
+    This implements the recurrent cell from the paper:
+    https://arxiv.org/abs/1611.01578
+    Barret Zoph and Quoc V. Le.
+    "Neural Architecture Search with Reinforcement Learning" Proc. ICLR 2017.
+
+    The class uses an optional projection layer.
+
+    # Arguments
+        units: Positive integer, dimensionality of the output space.
+        projection_units: (optional) Positive integer, The output dimensionality
+            for the projection matrices.  If None, no projection is performed.
+        activation: Activation function to use
+            (see [activations](../activations.md)).
+            If you pass None, no activation is applied
+            (ie. "linear" activation: `a(x) = x`).
+        recurrent_activation: Activation function to use
+            for the recurrent step
+            (see [activations](../activations.md)).
+        projection_activation: Activation function to use
+            for the projection step
+            (see [activations](../activations.md)).
+        use_bias: Boolean, whether the layer uses a bias vector.
+        kernel_initializer: Initializer for the `kernel` weights matrix,
+            used for the linear transformation of the inputs.
+            (see [initializers](../initializers.md)).
+        recurrent_initializer: Initializer for the `recurrent_kernel`
+            weights matrix,
+            used for the linear transformation of the recurrent state.
+            (see [initializers](../initializers.md)).
+        projection_initializer: Initializer for the `projection_kernel`
+            weights matrix,
+            used for the linear transformation of the projection step.
+            (see [initializers](../initializers.md)).
+        bias_initializer: Initializer for the bias vector
+            (see [initializers](../initializers.md)).
+        unit_forget_bias: Boolean.
+            If True, add 1 to the bias of the forget gate at initialization.
+            Setting it to true will also force `bias_initializer="zeros"`.
+            This is recommended in [Jozefowicz et al.](http://www.jmlr.org/proceedings/papers/v37/jozefowicz15.pdf)
+        kernel_regularizer: Regularizer function applied to
+            the `kernel` weights matrix
+            (see [regularizer](../regularizers.md)).
+        recurrent_regularizer: Regularizer function applied to
+            the `recurrent_kernel` weights matrix
+            (see [regularizer](../regularizers.md)).
+        projection_regularizer: Regularizer function applied to
+            the `projection_kernel` weights matrix
+            (see [regularizer](../regularizers.md)).
+        bias_regularizer: Regularizer function applied to the bias vector
+            (see [regularizer](../regularizers.md)).
+        kernel_constraint: Constraint function applied to
+            the `kernel` weights matrix
+            (see [constraints](../constraints.md)).
+        recurrent_constraint: Constraint function applied to
+            the `recurrent_kernel` weights matrix
+            (see [constraints](../constraints.md)).
+        projection_constraint: Constraint function applied to
+            the `projection_kernel` weights matrix
+            (see [constraints](../constraints.md)).
+        bias_constraint: Constraint function applied to the bias vector
+            (see [constraints](../constraints.md)).
+        dropout: Float between 0 and 1.
+            Fraction of the units to drop for
+            the linear transformation of the inputs.
+        recurrent_dropout: Float between 0 and 1.
+            Fraction of the units to drop for
+            the linear transformation of the recurrent state.
+        implementation: Implementation mode, either 1 or 2.
+            Mode 1 will structure its operations as a larger number of
+            smaller dot products and additions, whereas mode 2 will
+            batch them into fewer, larger operations. These modes will
+            have different performance profiles on different hardware and
+            for different applications.
+        return_sequences: Boolean. Whether to return the last output.
+            in the output sequence, or the full sequence.
+        return_state: Boolean. Whether to return the last state
+            in addition to the output.
+        go_backwards: Boolean (default False).
+            If True, process the input sequence backwards and return the
+            reversed sequence.
+        stateful: Boolean (default False). If True, the last state
+            for each sample at index i in a batch will be used as initial
+            state for the sample of index i in the following batch.
+        unroll: Boolean (default False).
+            If True, the network will be unrolled,
+            else a symbolic loop will be used.
+            Unrolling can speed-up a RNN,
+            although it tends to be more memory-intensive.
+            Unrolling is only suitable for short sequences.
+
+    # References
+        - [Long short-term memory](http://www.bioinf.jku.at/publications/older/2604.pdf) (original 1997 paper)
+        - [Learning to forget: Continual prediction with NestedLSTM](http://www.mitpressjournals.org/doi/pdf/10.1162/089976600300015015)
+        - [Supervised sequence labeling with recurrent neural networks](http://www.cs.toronto.edu/~graves/preprint.pdf)
+        - [A Theoretically Grounded Application of Dropout in Recurrent Neural Networks](http://arxiv.org/abs/1512.05287)
+        - [Nested LSTMs](https://arxiv.org/abs/1801.10308)
+    """
+
+    def __init__(self, units,
+                 projection_units=None,
+                 activation='tanh',
+                 recurrent_activation='sigmoid',
+                 projection_activation='linear',
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 recurrent_initializer='orthogonal',
+                 projection_initializer='glorot_uniform',
+                 bias_initializer='zeros',
+                 unit_forget_bias=True,
+                 kernel_regularizer=None,
+                 recurrent_regularizer=None,
+                 projection_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 recurrent_constraint=None,
+                 projection_constraint=None,
+                 bias_constraint=None,
+                 dropout=0.,
+                 recurrent_dropout=0.,
+                 implementation=2,
+                 return_sequences=False,
+                 return_state=False,
+                 go_backwards=False,
+                 stateful=False,
+                 unroll=False,
+                 **kwargs):
+        if implementation == 0:
+            warnings.warn('`implementation=0` has been deprecated, '
+                          'and now defaults to `implementation=2`.'
+                          'Please update your layer call.')
+        if K.backend() == 'theano':
+            warnings.warn(
+                'RNN dropout is no longer supported with the Theano backend '
+                'due to technical limitations. '
+                'You can either set `dropout` and `recurrent_dropout` to 0, '
+                'or use the TensorFlow backend.')
+            dropout = 0.
+            recurrent_dropout = 0.
+
+        cell = NASCell(units, projection_units,
+                       activation=activation,
+                       recurrent_activation=recurrent_activation,
+                       projection_activation=projection_activation,
+                       use_bias=use_bias,
+                       kernel_initializer=kernel_initializer,
+                       recurrent_initializer=recurrent_initializer,
+                       projection_initializer=projection_initializer,
+                       unit_forget_bias=unit_forget_bias,
+                       bias_initializer=bias_initializer,
+                       kernel_regularizer=kernel_regularizer,
+                       recurrent_regularizer=recurrent_regularizer,
+                       bias_regularizer=bias_regularizer,
+                       projection_regularizer=projection_regularizer,
+                       kernel_constraint=kernel_constraint,
+                       recurrent_constraint=recurrent_constraint,
+                       bias_constraint=bias_constraint,
+                       projection_constraint=projection_constraint,
+                       dropout=dropout,
+                       recurrent_dropout=recurrent_dropout,
+                       implementation=implementation)
+        super(NASRNN, self).__init__(cell,
+                                      return_sequences=return_sequences,
+                                      return_state=return_state,
+                                      go_backwards=go_backwards,
+                                      stateful=stateful,
+                                      unroll=unroll,
+                                      **kwargs)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+
+    def call(self, inputs, mask=None, training=None, initial_state=None, constants=None):
+        self.cell._dropout_mask = None
+        self.cell._recurrent_dropout_mask = None
+        return super(NASRNN, self).call(inputs,
+                                            mask=mask,
+                                            training=training,
+                                            initial_state=initial_state,
+                                            constants=constants)
+
+    @property
+    def units(self):
+        return self.cell.units
+
+    @property
+    def projection_units(self):
+        return self.cell.projection_units
+
+    @property
+    def activation(self):
+        return self.cell.activation
+
+    @property
+    def recurrent_activation(self):
+        return self.cell.recurrent_activation
+
+    @property
+    def projection_activation(self):
+        return self.cell.projection_activation
+
+    @property
+    def use_bias(self):
+        return self.cell.use_bias
+
+    @property
+    def kernel_initializer(self):
+        return self.cell.kernel_initializer
+
+    @property
+    def recurrent_initializer(self):
+        return self.cell.recurrent_initializer
+
+    @property
+    def bias_initializer(self):
+        return self.cell.bias_initializer
+
+    @property
+    def projection_initializer(self):
+        return self.cell.projection_initializer
+
+    @property
+    def unit_forget_bias(self):
+        return self.cell.unit_forget_bias
+
+    @property
+    def kernel_regularizer(self):
+        return self.cell.kernel_regularizer
+
+    @property
+    def recurrent_regularizer(self):
+        return self.cell.recurrent_regularizer
+
+    @property
+    def bias_regularizer(self):
+        return self.cell.bias_regularizer
+
+    @property
+    def projection_regularizer(self):
+        return self.cell.projection_regularizer
+
+    @property
+    def kernel_constraint(self):
+        return self.cell.kernel_constraint
+
+    @property
+    def recurrent_constraint(self):
+        return self.cell.recurrent_constraint
+
+    @property
+    def bias_constraint(self):
+        return self.cell.bias_constraint
+
+    @property
+    def projection_constraint(self):
+        return self.cell.projection_constraint
+
+    @property
+    def dropout(self):
+        return self.cell.dropout
+
+    @property
+    def recurrent_dropout(self):
+        return self.cell.recurrent_dropout
+
+    @property
+    def implementation(self):
+        return self.cell.implementation
+
+    def get_config(self):
+        config = {'units': self.units,
+                  'projection_units': self.projection_units,
+                  'activation': activations.serialize(self.activation),
+                  'recurrent_activation': activations.serialize(self.recurrent_activation),
+                  'projection_activation': activations.serialize(self.projection_activation),
+                  'use_bias': self.use_bias,
+                  'kernel_initializer': initializers.serialize(self.kernel_initializer),
+                  'recurrent_initializer': initializers.serialize(self.recurrent_initializer),
+                  'bias_initializer': initializers.serialize(self.bias_initializer),
+                  'projection_initializer': initializers.serialize(self.projection_initializer),
+                  'unit_forget_bias': self.unit_forget_bias,
+                  'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
+                  'recurrent_regularizer': regularizers.serialize(self.recurrent_regularizer),
+                  'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+                  'projection_regularizer': regularizers.serialize(self.projection_regularizer),
+                  'activity_regularizer': regularizers.serialize(self.activity_regularizer),
+                  'kernel_constraint': constraints.serialize(self.kernel_constraint),
+                  'recurrent_constraint': constraints.serialize(self.recurrent_constraint),
+                  'bias_constraint': constraints.serialize(self.bias_constraint),
+                  'projection_constraint': constraints.serialize(self.projection_constraint),
+                  'dropout': self.dropout,
+                  'recurrent_dropout': self.recurrent_dropout,
+                  'implementation': self.implementation}
+        base_config = super(NASRNN, self).get_config()
+        del base_config['cell']
+        return dict(list(base_config.items()) + list(config.items()))
+
+    @classmethod
+    def from_config(cls, config):
+        if 'implementation' in config and config['implementation'] == 0:
+            config['implementation'] = 2
+        return cls(**config)
+
+
+if __name__ == '__main__':
+    model = NASRNN(10, dropout=0.2, recurrent_dropout=0.2)
